96cf13f4967a0ed8174479a51d2d8a855f4035f2
ThamizhiniyanCS <csthamizhiniyan@gmail.com>
2025-02-20 23:13:46 +0530

    Initial Commit

---
+++ astro.config.mjs
@@ -0,0 +1,44 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import tailwind from "@astrojs/tailwind";
+import starlightImageZoom from "starlight-image-zoom";
+import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "Thamizhiniyan C S",
+      description:
+        "A collection and configuration of lab environments that I use.",
+      social: {
+        github: "https://github.com/ThamizhiniyanCS",
+        linkedin: "https://www.linkedin.com/in/ThamizhiniyanCS/",
+      },
+      sidebar: [
+        {
+          label: "Malware Analysis Lab Setup",
+          autogenerate: {
+            directory: "malware-analysis-lab-setup",
+          },
+        },
+      ],
+      customCss: ["./src/tailwind.css"],
+      tableOfContents: { minHeadingLevel: 2, maxHeadingLevel: 4 },
+      components: {
+        Header: "./src/overrides/Header.astro",
+      },
+      expressiveCode: {
+        styleOverrides: { borderRadius: "0.5rem" },
+      },
+      plugins: [starlightImageZoom()],
+      lastUpdated: true,
+    }),
+    tailwind({ applyBaseStyles: false }),
+    react(),
+    sitemap(),
+  ],
+});
